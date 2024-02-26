29715596b19027714d4fc2b48e1ebccac6c4d34a
ACTG802 <thudson686@gmail.com>
2024-02-26 07:29:04 -0800

    updated dropout layer to keras to avoid warnings deepcas9_models.py

--- src/smk/pipelines/py/deepcas_models.py
+++ src/smk/pipelines/py/deepcas_models.py
@@ -1,4 +1,5 @@
 import tensorflow.compat.v1 as tf1
+import tensorflow as tf
 
 
 
@@ -23,18 +24,20 @@ class DeepCas9(object):
             bias = tf1.Variable(tf1.truncated_normal([num_filters]), name=name + '_b')
 
             # setup the convolutional layer operation
-            out_layer = tf1.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='VALID')
+            out_layer = tf.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='VALID')
+            #out_layer = tf1.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='VALID')
 
             # add the bias
             out_layer += bias
 
             # apply a ReLU non-linear activation
-            out_layer = tf1.layers.dropout(tf1.nn.relu(out_layer), 0.3, self.is_training)
+            #out_layer = tf1.layers.dropout(tf1.nn.relu(out_layer), 0.3, self.is_training)
+            out_layer = tf.keras.layers.Dropout(rate=0.3)(tf.nn.relu(out_layer))
 
             # now perform max pooling
             ksize = [1, pool_shape[0], pool_shape[1], 1]
             strides = [1, 1, 2, 1]
-            out_layer = tf1.nn.avg_pool(out_layer, ksize=ksize, strides=strides,
+            out_layer = tf.nn.avg_pool(out_layer, ksize=ksize, strides=strides,
                                         padding='SAME')
             return out_layer
 
@@ -43,6 +46,7 @@ class DeepCas9(object):
         L_pool_0 = create_new_conv_layer(self.inputs, 4, filter_num[0], [1, filter_size[0]], [1, 2], name='conv1')
         L_pool_1 = create_new_conv_layer(self.inputs, 4, filter_num[1], [1, filter_size[1]], [1, 2], name='conv2')
         L_pool_2 = create_new_conv_layer(self.inputs, 4, filter_num[2], [1, filter_size[2]], [1, 2], name='conv3')
+
         with tf1.variable_scope('Fully_Connected_Layer1'):
             layer_node_0 = int((length - filter_size[0]) / 2) + 1
             node_num_0 = layer_node_0 * filter_num[0]
@@ -50,31 +54,35 @@ class DeepCas9(object):
             node_num_1 = layer_node_1 * filter_num[1]
             layer_node_2 = int((length - filter_size[2]) / 2) + 1
             node_num_2 = layer_node_2 * filter_num[2]
-            L_flatten_0 = tf1.reshape(L_pool_0, [-1, node_num_0])
-            L_flatten_1 = tf1.reshape(L_pool_1, [-1, node_num_1])
-            L_flatten_2 = tf1.reshape(L_pool_2, [-1, node_num_2])
-            L_flatten = tf1.concat([L_flatten_0, L_flatten_1, L_flatten_2], 1, name='concat')
+
+            L_flatten_0 = tf.reshape(L_pool_0, [-1, node_num_0])
+            L_flatten_1 = tf.reshape(L_pool_1, [-1, node_num_1])
+            L_flatten_2 = tf.reshape(L_pool_2, [-1, node_num_2])
+            L_flatten = tf.concat([L_flatten_0, L_flatten_1, L_flatten_2], 1, name='concat')
+
             node_num = node_num_0 + node_num_1 + node_num_2
             W_fcl1 = tf1.get_variable("W_fcl1", shape=[node_num, node_1])
             B_fcl1 = tf1.get_variable("B_fcl1", shape=[node_1])
-            L_fcl1_pre = tf1.nn.bias_add(tf1.matmul(L_flatten, W_fcl1), B_fcl1)
-            L_fcl1 = tf1.nn.relu(L_fcl1_pre)
-            L_fcl1_drop = tf1.layers.dropout(L_fcl1, 0.3, self.is_training)
+            L_fcl1_pre   = tf.nn.bias_add(tf.matmul(L_flatten, W_fcl1), B_fcl1)
+            L_fcl1       = tf.nn.relu(L_fcl1_pre)
+            L_fcl1_drop  = tf.keras.layers.Dropout(rate=0.3)(L_fcl1)
+
 
         with tf1.variable_scope('Fully_Connected_Layer2'):
             W_fcl2 = tf1.get_variable("W_fcl2", shape=[node_1, node_2])
             B_fcl2 = tf1.get_variable("B_fcl2", shape=[node_2])
-            L_fcl2_pre = tf1.nn.bias_add(tf1.matmul(L_fcl1_drop, W_fcl2), B_fcl2)
-            L_fcl2 = tf1.nn.relu(L_fcl2_pre)
-            L_fcl2_drop = tf1.layers.dropout(L_fcl2, 0.3, self.is_training)
+            L_fcl2_pre = tf.nn.bias_add(tf.matmul(L_fcl1_drop, W_fcl2), B_fcl2)
+            L_fcl2 = tf.nn.relu(L_fcl2_pre)
+            L_fcl2_drop = tf.keras.layers.Dropout(rate=0.3)(L_fcl2)
+
 
         with tf1.variable_scope('Output_Layer'):
             W_out = tf1.get_variable("W_out",
                                      shape=[node_2, 1])  # , initializer=tf.contrib.layers.xavier_initializer())
             B_out = tf1.get_variable("B_out", shape=[1])  # , initializer=tf.contrib.layers.xavier_initializer())
-            self.outputs = tf1.nn.bias_add(tf1.matmul(L_fcl2_drop, W_out), B_out)
+            self.outputs = tf.nn.bias_add(tf.matmul(L_fcl2_drop, W_out), B_out)
 
         # Define loss function and optimizer
-        self.obj_loss = tf1.reduce_mean(tf1.square(self.targets - self.outputs))
+        self.obj_loss = tf.reduce_mean(tf.square(self.targets - self.outputs))
         self.optimizer = tf1.train.AdamOptimizer(l_rate).minimize(self.obj_loss)
     # def end: def __init__
