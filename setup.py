7b3cbae357d37242d6cb898ef24ae0e042fe2fcf
dbellieny <daniel.bellieny@gmail.com>
2024-05-30 17:28:11 -0700

    updates on setup + release of v0.2.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='meditability',
-    version='0.2.4',
+    version='0.2.6',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
