28d7e87fcfe448f4b32808b2f4554db8f1295316
dbellieny <daniel.bellieny@gmail.com>
2025-02-07 02:05:50 -0800

    0.4.1 - Uploaded on pyPI

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='meditability',
-    version='0.4.0',
+    version='0.4.1',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
